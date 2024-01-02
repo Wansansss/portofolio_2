11491338574a9f0a92609cc328e50bd9714afc67
Wansansss <ridhwanardiansyah@gmail.com>
2024-01-02 18:12:51 +0700

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -23,7 +23,7 @@ function SideBar() {
             : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
         }>
             <div className="gap-4 p-4 flex flex-col h-screen overflow-y-auto">  
-        <div className="flex justify-end items-center hover:text-pink-500 cursor-pointer text-white ">
+        <div className="flex justify-end items-center hover:text-blue-700 cursor-pointer text-white ">
         <X size={20} onClick={handleNav} />
         </div>  
         <Image
@@ -34,47 +34,47 @@ function SideBar() {
           height={350}
         />
           <h3 className=" justify-center text-center mb-10 text-md text-white">Ridhwan Ardiyansyah</h3>
-          <div className="flex hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out">
+          <div className="flex hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out">
             <House size={26} weight="fill" />
             <Link href={"/"}>
               Home
             </Link>
           </div>
-          <div className="flex hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out">
+          <div className="flex hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out">
             <IdentificationCard size={26} weight="fill" />
             <Link href={"#about"}>
               About Me
             </Link>
           </div>
-          <div className="flex hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out">
+          <div className="flex hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out">
             <ReadCvLogo size={26} weight="fill" />
             <Link href={"#resume"}>
               Resume
             </Link>
           </div>
-          <div className="flex hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out">
+          <div className="flex hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out">
           <CubeTransparent size={26} weight="fill" />
             <Link href={"#project"}>
               Projects
             </Link>
           </div>
-          <div className="flex hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out">
+          <div className="flex hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out">
           <AddressBook size={26} weight="fill" />
             <Link href={"#contact"}>
               Contact Me
             </Link>
           </div>
           <div className="flex flex-row justify-center gap-2 mt-20 ">
-          <Link href="https://www.instagram.com/rdwnardynsyh" className="flex  hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out" >
+          <Link href="https://www.instagram.com/rdwnardynsyh" className="flex  hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out" >
         <InstagramLogo size={26} />
         </Link>
-        <Link href="" className="flex  hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out">
+        <Link href="" className="flex  hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out">
         <LinkedinLogo size={26} />
         </Link>
-        <Link href="mailto:ridhwanardiansyah@gmail.com" className="flex  hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out">
+        <Link href="mailto:ridhwanardiansyah@gmail.com" className="flex  hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out">
         <Envelope size={28} />
         </Link>
-        <Link href="https://wa.me/6285710093376" className="flex  hover:text-pink-500 cursor-pointer text-white transition-all duration-200 ease-out">
+        <Link href="https://wa.me/6285710093376" className="flex  hover:text-blue-700 cursor-pointer text-white transition-all duration-200 ease-out">
         <WhatsappLogo size={28} />
         </Link>
         
