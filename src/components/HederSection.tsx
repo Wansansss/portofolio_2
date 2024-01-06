cfb8f131f589de6d7f5a2f29ad0629b6b0ba2af7
Wansansss <ridhwanardiansyah@gmail.com>
2024-01-06 13:32:53 +0700

    Update HederSection.tsx

--- src/components/HederSection.tsx
+++ src/components/HederSection.tsx
@@ -4,16 +4,17 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { Typewriter } from "react-simple-typewriter";
 
-
 function HederSection() {
   const [message] = useState(["Universitas", "Tarumanagara"]);
-  const [name] = useState(["Ridhwan Ardiyansyah"])
+  const [name] = useState(["Ridhwan Ardiyansyah"]);
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center">
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-emerald-700">Hello, I'm {" "}</div>
+            <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-emerald-700">
+              Hello, I'm{" "}
+            </div>
             <Typewriter words={name} loop={false} cursor />
           </h1>
           <h1 className="font-extrabold text-white mb-4 text-xl lg:text-2xl">
@@ -26,15 +27,28 @@ function HederSection() {
             online stores.
           </p>
           <div>
-            <button
-              className="text-black hover:text-white bg-white hover:bg-blue-700  rounded-full w-full sm:w-fit px-4 py-4 shadow-xl shadow-blue-700 hover:shadow-white mr-4"
-            >
-              Hire Me
-            </button>
             <Link
-           href={'/img/my_cv.pdf'}  target="_blank"  rel="noopener noreferrer" locale={false} download className="text-black hover:text-white bg-white hover:bg-blue-700 rounded-full w-full sm:w-fit px-4 py-4 shadow-xl shadow-blue-700 hover:shadow-white mt-4"
+              href={"mailto:ridhwanardiansyah@gmail.com"}
+              target="_blank"
+              rel="noopener noreferrer"
+              locale={false}
+              download
             >
-              Download CV
+              <button className="text-black hover:text-white bg-white hover:bg-blue-700  rounded-full w-full sm:w-fit px-4 py-4 shadow-xl shadow-blue-700 hover:shadow-white mr-4">
+                Hire Me
+              </button>
+            </Link>
+
+            <Link
+              href={"/img/my_cv.pdf"}
+              target="_blank"
+              rel="noopener noreferrer"
+              locale={false}
+              download
+            >
+              <button className="text-black hover:text-white bg-white hover:bg-blue-700 rounded-full w-full sm:w-fit px-4 py-4 shadow-xl shadow-blue-700 hover:shadow-white mt-4">
+                Download CV
+              </button>
             </Link>
           </div>
         </div>
