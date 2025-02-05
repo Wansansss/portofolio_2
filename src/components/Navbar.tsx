afecea7dee40901948635cb77dcd1cae4de4e680
Wansansss <ridhwanardiansyah@gmail.com>
2025-02-05 15:12:18 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,12 +21,8 @@ const navLinks = [
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
-      <div className="flex flex-wrap items-center justify-between mx-auto gap-4">
-        <Link href={"/"} className="text-2xl md:text-4xl text-white font-semibold">
-          Wansans
-        </Link>
-
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#6d6d6d] bg-opacity-100">
+      <div className="flex flex-wrap items-center justify-center mx-auto gap-4">
         <SideBar />
        
         <div className="menu hidden md:block md:w-auto" id="navbar">
